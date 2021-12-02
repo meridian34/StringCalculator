f3b89812a569b570878afa8d2dd695b22e34bedf
a.sobol <sobol.andrey.25@gmail.com>
2021-12-02 15:46:48 +0200

    add method and logic for task 1

--- service/Task1.StringCalculator/Services/StringCalculatorService.cs
+++ service/Task1.StringCalculator/Services/StringCalculatorService.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Task1.StringCalculator.Services
 {
-    class StringCalculatorService
+    public class StringCalculatorService
     {
+        public int Sum(string numbers)
+        {
+            if (string.IsNullOrWhiteSpace(numbers))
+            {
+                return 0;
+            }
+            var separatedSymbols = numbers.Split(',');
+            var sum = 0;
+            try
+            {
+                foreach (var i in separatedSymbols)
+                {
+                    sum += int.Parse(i);
+                }
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+            return sum;
+        }
     }
 }
