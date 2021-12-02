0bcf57b43949b11fcedfd8cb42e302b1c1c73660
a.sobol <sobol.andrey.25@gmail.com>
2021-12-02 17:48:57 +0200

    tests fix

---
+++ tests/StringCalculator.Tests/UnitTests.cs
@@ -0,0 +1,77 @@
+using FluentAssertions;
+using System;
+using StringCalculator.Services;
+using Xunit;
+
+namespace StringCalculator.Tests
+{
+    public class UnitTests
+    {
+        private readonly StringCalculatorService _service = new StringCalculatorService();
+
+        [Fact]
+        public void Task1_1_Sum_Success()
+        {
+            //arrange
+            var numbers = "2,3";            
+
+            //act
+            var result = _service.Sum(numbers);
+
+            //assert
+            result.Should().Be(5);
+        }
+
+        [Fact]
+        public void Task1_2_Sum_Success()
+        {
+            //arrange
+            var numbers = "";            
+
+            //act
+            var result = _service.Sum(numbers);
+
+            //assert
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void Task1_3_Sum_Success()
+        {
+            //arrange
+            var numbers = "1";
+
+            //act
+            var result = _service.Sum(numbers);
+
+            //assert
+            result.Should().Be(1);
+        }
+
+        [Fact]
+        public void Test2_Sum_Success()
+        {
+            //arrange
+            var numbers = "2,3,4,5";
+
+            //act
+            var result = _service.Sum(numbers);
+
+            //assert
+            result.Should().Be(14);
+        }
+
+        [Fact]
+        public void Test3_Sum_Success()
+        {
+            //arrange
+            var numbers = "1\n2,3";
+
+            //act
+            var result = _service.Sum(numbers);
+
+            //assert
+            result.Should().Be(6);
+        }
+    }
+}
