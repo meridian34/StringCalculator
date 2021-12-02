4e3d250424b16f5cebe35143c16c6a1d4bcc3eff
a.sobol <sobol.andrey.25@gmail.com>
2021-12-02 15:56:44 +0200

    added test for task 1

--- tests/Task1.StringCalculator.Test/UnitTest1.cs
+++ tests/Task1.StringCalculator.Test/UnitTest1.cs
@@ -1,4 +1,6 @@
+using FluentAssertions;
 using System;
+using Task1.StringCalculator.Services;
 using Xunit;
 
 namespace Task1.StringCalculator.Test
@@ -8,7 +10,43 @@ namespace Task1.StringCalculator.Test
         [Fact]
         public void Test1_Success()
         {
+            //arrange
+            var numbers = "2,3";
+            var service = new StringCalculatorService();
 
+            //act
+            var result = service.Sum(numbers);
+
+            //assert
+            result.Should().Be(5);
+        }
+
+        [Fact]
+        public void Test2_Success()
+        {
+            //arrange
+            var numbers = "";
+            var service = new StringCalculatorService();
+
+            //act
+            var result = service.Sum(numbers);
+
+            //assert
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test3_Success()
+        {
+            //arrange
+            var numbers = "1";
+            var service = new StringCalculatorService();
+
+            //act
+            var result = service.Sum(numbers);
+
+            //assert
+            result.Should().Be(1);
         }
     }
 }
