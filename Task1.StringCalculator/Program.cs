ba9b16f0a93cc25092db097f203c8754523cb831
a.sobol <a.sobol@DESKTOP-5NI0SMB>
2021-12-02 15:33:12 +0200

    Add project files.

---
+++ Task1.StringCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1.StringCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
