f5ba95451dcec4d1fe1a9c8d3552d27221dcb843
a.sobol <sobol.andrey.25@gmail.com>
2021-12-02 17:13:30 +0200

    modified logic for task 4

--- service/Task1.StringCalculator/Services/StringCalculatorService.cs
+++ service/Task1.StringCalculator/Services/StringCalculatorService.cs
@@ -8,17 +8,54 @@ namespace Task1.StringCalculator.Services
 {
     public class StringCalculatorService
     {
-        public int Sum(string numbers)
+        private readonly string _customDelimiterStartMarker = "//";
+        private readonly string _customDelimiterEndMarker = "\n";
+        private readonly string[] _defaultDelimiters = { ",", "\n" };
+        private string _customDelimiter;
+
+        public int Sum(string data)
         {
-            if (string.IsNullOrWhiteSpace(numbers))
+            if (string.IsNullOrWhiteSpace(data))
             {
                 return 0;
             }
-            var separatedSymbols = numbers.Split(',','\n');
+
+            if (IsCustomDelimiter(data))
+            {
+                _customDelimiter = GetCustomDelimiter(data);
+                return GetSum(GetNumbers(data, _customDelimiter));
+            }
+
+            return GetSum(GetNumbers(data, _defaultDelimiters));
+            
+        }
+
+        private bool IsCustomDelimiter(string data)
+        {
+            return data.Contains(_customDelimiterStartMarker) && data.Contains(_customDelimiterEndMarker);
+        }
+
+        private string GetCustomDelimiter(string data)
+        {
+            var separatedData = data.Split(_customDelimiterEndMarker);
+            return separatedData[0].Replace(_customDelimiterStartMarker, string.Empty);
+        }
+
+        private IReadOnlyCollection<string> GetNumbers(string data, IReadOnlyCollection<string> delimiters)
+        {
+            return data.Split(delimiters.ToArray<string>(), StringSplitOptions.None);            
+        }
+        private IReadOnlyCollection<string> GetNumbers(string data, string delimiter)
+        {
+            return data.Split(delimiter);
+        }
+
+        private int GetSum(IReadOnlyCollection<string> numbers)
+        {
             var sum = 0;
             try
             {
-                foreach (var i in separatedSymbols)
+                foreach (var i in numbers)
                 {
                     sum += int.Parse(i);
                 }
