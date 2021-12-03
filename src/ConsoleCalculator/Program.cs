65273333f5f1ea2a97b8dae5619056372c643788
a.sobol <sobol.andrey.25@gmail.com>
2021-12-03 16:52:54 +0200

    Added ConsoleCalculator project and change path for this project

---
+++ src/ConsoleCalculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleCalculator.Services;
+using System;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new ConsoleService();
+            service.Start();
+        }
+    }
+}
