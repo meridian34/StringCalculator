c4b05b832ca00dbafa830118a698a769b1590723
a.sobol <sobol.andrey.25@gmail.com>
2021-12-06 11:46:30 +0200

    Added Moq test

---
+++ ConsoleCalculator.Tests/ConsoleCalculatorServiceTests.cs
@@ -0,0 +1,51 @@
+using ConsoleCalculator.Services;
+using ConsoleCalculator.Services.Abstractions;
+using Moq;
+using StringCalculator.Services.Abstractions;
+using System;
+using Xunit;
+
+namespace ConsoleCalculator.Tests
+{
+    public class ConsoleCalculatorServiceTests
+    {
+        private readonly Mock<IConsoleService> _consoleService;
+        private readonly Mock<IStringCalculatorService> _stringCalculatorService;
+        private IConsoleCalculateService _consoleCalculateService;
+
+        public ConsoleCalculatorServiceTests()
+        {
+            
+            _consoleService = new Mock<IConsoleService>();
+            _stringCalculatorService = new Mock<IStringCalculatorService>();
+        }
+
+        [Fact]
+        public void Start_InvokeStartMethod_ShouldVerified()
+        {
+            //arrange
+            _consoleService.SetupSequence(x => x.ReadLine())
+                .Returns("1,2")
+                .Returns("4,5")
+                .Returns("");
+
+            _stringCalculatorService.SetupSequence(x => x.Sum(It.Is<string>(s => s == "1,2" || s == "4,5")))
+                .Returns(3)
+                .Returns(9);
+
+            _consoleCalculateService = new ConsoleCalculateService(_stringCalculatorService.Object, _consoleService.Object);
+
+            //act
+            
+            _consoleCalculateService.Start();
+
+            //assert
+            _consoleService.Verify(x => x.WriteLine(It.Is<string>(s => s == "3" ||
+                                                                        s == "9" ||
+                                                                        s == "Enter comma separated numbers (enter to exit):" ||
+                                                                        s == "Result is: 3" ||
+                                                                        s == "Result is: 9" ||
+                                                                        s == "you can enter other numbers (enter to exit)?")), Times.AtLeast(5));
+        }
+    }
+}
